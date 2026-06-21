92b97358297638cafb03e4de2155c72426e9823e
dembele00a <dembelem@proton.me>
2026-06-21 17:32:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("Zaqel Mix Bot Started")
+import os
+import requests
+import time
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+def send(chat_id, text):
+    requests.get(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        params={"chat_id": chat_id, "text": text}
+    )
+
+print("Bot started")
+
+while True:
+    time.sleep(60)
