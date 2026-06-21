24037aea74a9cda7e35a0ad86af06ecbdfb29869
dembele00a <dembelem@proton.me>
2026-06-21 17:58:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 import os
-import requests
 import time
+import requests
 
 TOKEN = os.getenv("BOT_TOKEN")
+OFFSET = None
 
 def send(chat_id, text):
     requests.get(
         f"https://api.telegram.org/bot{TOKEN}/sendMessage",
-        params={"chat_id": chat_id, "text": text}
+        params={
+            "chat_id": chat_id,
+            "text": text
+        }
     )
 
 print("Bot started")
 
 while True:
-    time.sleep(60)
+    try:
+        r = requests.get(
+            f"https://api.telegram.org/bot{TOKEN}/getUpdates",
+            params={"offset": OFFSET, "timeout": 20}
+        ).json()
+
+        for u in r.get("result", []):
+            OFFSET = u["update_id"] + 1
+
+            if "message" in u:
+                chat_id = u["message"]["chat"]["id"]
+                text = u["message"].get("text", "")
+
+                if text == "/start":
+                    send(
+                        chat_id,
+                        "👋 Zaqel aktif.\n\n1. Kripto → Kripto\n2. IBAN → Kripto\n3. Kripto → IBAN"
+                    )
+
+        time.sleep(1)
+
+    except Exception as e:
+        print(e)
+        time.sleep(5)
